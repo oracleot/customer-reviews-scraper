1ba315139b3c949719d24fb215735b700830a604
Damilola Oduronbi <doduronbi@gmail.com>
2020-10-01 17:33:19 +0100

    Fix rubocop errors: freeze mutable objects assigned to constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,3 +1,3 @@
-BASE_MOVIE_URL = 'https://www.rottentomatoes.com/m/'
-AUDIENCE_REVIEW_SELECTOR = '.mop-audience-reviews__reviews-wrap .mop-audience-reviews__review--comment'
-CRITIC_REVIEW_SELECTOR = '.top_critic p'
+BASE_MOVIE_URL = 'https://www.rottentomatoes.com/m/'.freeze
+AUDIENCE_REVIEW_SELECTOR = '.mop-audience-reviews__reviews-wrap .mop-audience-reviews__review--comment'.freeze
+CRITIC_REVIEW_SELECTOR = '.top_critic p'.freeze
--- lib/spider.rb
+++ lib/spider.rb
@@ -4,7 +4,7 @@ require_relative '../lib/constants.rb'
 
 class Spider
   def initialize(url = 'billy_the_kid_2007')
-    endpoint_url = (BASE_MOVIE_URL+url).to_s
+    endpoint_url = (BASE_MOVIE_URL + url).to_s
     @doc = Nokogiri::HTML(URI.open(endpoint_url))
   end
 
